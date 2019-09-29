e0e71d94dee51ffd327d320c1b942530e0639bd8
nannany <ymym1990ymym@gmail.com>
2019-09-29 18:18:09 +0900

    cretate functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
-function App() {
-    return (
-        <React.Fragment>
-            <label htmlFor="bar">bar</label>
-            <input type="text" onChange={() => {
-                console.log("im clicked")
-            }}/>
-        </React.Fragment>
-    )
+// function App() {
+//     return (
+//         <React.Fragment>
+//             <label htmlFor="bar">bar</label>
+//             <input type="text" onChange={() => {
+//                 console.log("im clicked")
+//             }}/>
+//         </React.Fragment>
+//     )
+// }
+
+const App = () => {
+    return <div>
+        <Cat/>
+        <Cat/>
+        <Cat/>
+        <Cat/>
+
+    </div>
+
+}
+
+const Cat = () => {
+    return <div>Meow</div>
 }
 
 export default App;
