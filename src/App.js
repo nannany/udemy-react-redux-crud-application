6381375f602b3d344395e77469186d9a31c73501
nannany <ymym1990ymym@gmail.com>
2019-09-29 17:25:29 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div >
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.hello-world
-        </p>
-      </header>
-    </div>
+  return React.createElement(
+      "h1",
+      null,
+      "hello world"
   );
 }
 
